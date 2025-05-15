2fed25bc94c5808763db395b1859379081d51ece
MuneebAhmad03 <muneeebahmad.03@gmail.com>
2025-05-15 20:54:21 +0500

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,14 @@
+public class Encapsulation {
+
+    private int age1;                          //Access in Class only     need getter and setter
+    private String IDNumber;
+
+    protected int age2;                        //Access in class as well as subclasses
+    protected String name;
+
+    public int age3;                             //Access in all
+    public int name2;
+
+
+
+}
